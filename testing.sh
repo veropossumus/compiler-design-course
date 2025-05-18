9e337ff4d205996ceef7c7ea4c9848391f0368a3
Ge-sa <schoenbohm.gesa@t-online.de>
2025-05-18 18:24:57 +0200

    fix test

--- testing.sh
+++ testing.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 BIN_DIR="$(dirname "$0")/build/install/compiler/bin"
-$BIN_DIR/compiler test.md output.s
-gcc output.s -o output
+$BIN_DIR/compiler test.md output
 ./output
 echo $?
 
