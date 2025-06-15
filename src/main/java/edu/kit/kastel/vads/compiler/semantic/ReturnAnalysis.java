3d3e7427c669bdf9ec1ceace8dfd2608ff5c0bd1
Ge-sa <schoenbohm.gesa@t-online.de>
2025-06-15 21:03:03 +0200

    fix return analysis

--- src/main/java/edu/kit/kastel/vads/compiler/semantic/ReturnAnalysis.java
+++ src/main/java/edu/kit/kastel/vads/compiler/semantic/ReturnAnalysis.java
@@ -1,7 +1,6 @@
 package edu.kit.kastel.vads.compiler.semantic;
 
-import edu.kit.kastel.vads.compiler.parser.ast.FunctionTree;
-import edu.kit.kastel.vads.compiler.parser.ast.ReturnTree;
+import edu.kit.kastel.vads.compiler.parser.ast.*;
 import edu.kit.kastel.vads.compiler.parser.visitor.NoOpVisitor;
 import edu.kit.kastel.vads.compiler.parser.visitor.Unit;
 
@@ -21,10 +20,50 @@ class ReturnAnalysis implements NoOpVisitor<ReturnAnalysis.ReturnState> {
 
     @Override
     public Unit visit(FunctionTree functionTree, ReturnState data) {
-        if (!data.returns) {
+        if (!hasReturn(functionTree.body())) {
             throw new SemanticException("function " + functionTree.name() + " does not return");
         }
-        data.returns = false;
         return NoOpVisitor.super.visit(functionTree, data);
+    }
+
+    private boolean hasReturn(StatementTree statement) {
+        ReturnState state = new ReturnState();
+        statement.accept(this, state);
+        return state.returns;
+    }
+
+
+    @Override
+    public Unit visit(IfTree ifTree, ReturnState data) {
+        boolean originalReturns = data.returns;
+
+        data.returns = false;
+        ifTree.thenBranch().accept(this, data);
+        boolean thenReturns = data.returns;
+
+        boolean elseReturns = false;
+        if (ifTree.elseBranch() != null) {
+            data.returns = false;
+            ifTree.elseBranch().accept(this, data);
+            elseReturns = data.returns;
+
+            data.returns = originalReturns || (thenReturns && elseReturns);
+        } else {
+            data.returns = originalReturns;
+        }
+        return NoOpVisitor.super.visit(ifTree, data);
+    }
+
+    @Override
+    public Unit visit(BlockTree blockTree, ReturnState data) {
+        for (StatementTree statement : blockTree.statements()) {
+            statement.accept(this, data);
+
+            if (data.returns) {
+                break;
+            }
+        }
+
+        return NoOpVisitor.super.visit(blockTree, data);
     }
 }
